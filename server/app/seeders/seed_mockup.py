d1181d221cb85377d5254f0ed95184337ad78e8a
Fabián Torres <23241003@leon.tecnm.mx>
2026-06-09 23:35:48 -0600

    Actualizar seeders

--- server/app/seeders/seed_mockup.py
+++ server/app/seeders/seed_mockup.py
@@ -22,10 +22,13 @@ def seed(db: Session):
 
     # 2. Crear Grupos de Modificadores Mockup
     grupo_carne = models.GrupoModificador(
-        nombre="Carne", minimo=1, maximo=1, categoria_id=cat_alimentos.id
+        nombre="Carne", minimo=1, maximo=2, categoria_id=cat_alimentos.id
     )
     grupo_salsa = models.GrupoModificador(
-        nombre="Salsa", minimo=0, maximo=2, categoria_id=cat_alimentos.id
+        nombre="Salsa", minimo=0, maximo=0, categoria_id=cat_alimentos.id
+    )
+    grupo_verdura = models.GrupoModificador(
+        nombre="Verdura", minimo=0, maximo=0, categoria_id=cat_alimentos.id
     )
     grupo_tortilla = models.GrupoModificador(
         nombre="Tortilla", minimo=1, maximo=1, categoria_id=cat_alimentos.id
@@ -34,26 +37,35 @@ def seed(db: Session):
         nombre="Tamaño", minimo=1, maximo=1, categoria_id=cat_bebidas.id
     )
     
-    db.add_all([grupo_carne, grupo_salsa, grupo_tortilla, grupo_tamano])
+    db.add_all([grupo_carne, grupo_salsa, grupo_verdura, grupo_tortilla, grupo_tamano])
     db.commit()
     db.refresh(grupo_carne)
     db.refresh(grupo_salsa)
+    db.refresh(grupo_verdura)
     db.refresh(grupo_tortilla)
     db.refresh(grupo_tamano)
 
-    # 3. Crear Opciones para Modificadores (sólo para que tengan algo de sentido, aunque no son tan críticas aquí)
+    # 3. Crear Opciones para Modificadores
     opciones = [
-        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Pastor", precio_extra=0.0),
-        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Asada", precio_extra=5.0),
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Pastor", precio_extra=0.0), # 0
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Asada", precio_extra=5.0), # 1
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Bistec", precio_extra=0.0), # 2
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Chorizo", precio_extra=0.0), # 3
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Costilla", precio_extra=0.0), # 4
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Cabeza", precio_extra=0.0), # 5
+        models.OpcionModificador(grupo_id=grupo_carne.id, nombre="Tripa", precio_extra=2.0), # 6
         
-        models.OpcionModificador(grupo_id=grupo_salsa.id, nombre="Salsa Roja", precio_extra=0.0),
-        models.OpcionModificador(grupo_id=grupo_salsa.id, nombre="Salsa Verde", precio_extra=0.0),
+        models.OpcionModificador(grupo_id=grupo_salsa.id, nombre="Roja", precio_extra=0.0), # 7
+        models.OpcionModificador(grupo_id=grupo_salsa.id, nombre="Verde", precio_extra=0.0), # 8
+
+        models.OpcionModificador(grupo_id=grupo_verdura.id, nombre="Cebolla", precio_extra=0.0), # 9
+        models.OpcionModificador(grupo_id=grupo_verdura.id, nombre="Cilantro", precio_extra=0.0), # 10
         
-        models.OpcionModificador(grupo_id=grupo_tortilla.id, nombre="Maíz", precio_extra=0.0),
-        models.OpcionModificador(grupo_id=grupo_tortilla.id, nombre="Harina", precio_extra=2.0),
+        models.OpcionModificador(grupo_id=grupo_tortilla.id, nombre="Maíz", precio_extra=0.0), # 11
+        models.OpcionModificador(grupo_id=grupo_tortilla.id, nombre="Harina", precio_extra=2.0), # 12
         
-        models.OpcionModificador(grupo_id=grupo_tamano.id, nombre="Chico", precio_extra=0.0),
-        models.OpcionModificador(grupo_id=grupo_tamano.id, nombre="Grande", precio_extra=10.0),
+        models.OpcionModificador(grupo_id=grupo_tamano.id, nombre="Chico", precio_extra=0.0), # 13
+        models.OpcionModificador(grupo_id=grupo_tamano.id, nombre="Grande", precio_extra=10.0), # 14
     ]
     db.add_all(opciones)
     db.commit()
@@ -95,14 +107,14 @@ def seed(db: Session):
     ]
     
     # Asignar modificadores
-    productos[0].modificadores = [grupo_carne, grupo_salsa] # Taco
-    productos[1].modificadores = [grupo_carne, grupo_tortilla, grupo_salsa] # Quesadilla
-    productos[2].modificadores = [grupo_carne, grupo_salsa] # Torta
-    productos[3].modificadores = [grupo_carne, grupo_salsa] # Volcan
+    productos[0].modificadores = [grupo_carne, grupo_salsa, grupo_verdura] # Taco
+    productos[1].modificadores = [grupo_carne, grupo_tortilla, grupo_salsa, grupo_verdura] # Quesadilla
+    productos[2].modificadores = [grupo_carne, grupo_salsa, grupo_verdura] # Torta
+    productos[3].modificadores = [grupo_carne, grupo_salsa, grupo_verdura] # Volcan
     productos[4].modificadores = [] # Refresco vidrio
     productos[5].modificadores = [] # Refresco taparrosca
     productos[6].modificadores = [grupo_tamano] # Agua de sabor
-    productos[7].modificadores = [grupo_carne, grupo_salsa] # Taco de tripa
+    productos[7].modificadores = [grupo_carne, grupo_salsa, grupo_verdura] # Taco de tripa
     
     db.add_all(productos)
     db.commit()
@@ -134,7 +146,7 @@ def seed(db: Session):
         if i % 2 == 0:
             det = models.DetalleOrden(id_producto=productos[0].id, cantidad=3, subtotal=17.0 * 3)
             # Agregar modificadores al taco: Pastor y Salsa Verde
-            det.opciones = [opciones[0], opciones[3]]
+            det.opciones = [opciones[0], opciones[8], opciones[10]]
             detalles.append(det)
 
             det2 = models.DetalleOrden(id_producto=productos[4].id, cantidad=2, subtotal=21.0 * 2)
@@ -143,7 +155,7 @@ def seed(db: Session):
         else:
             det = models.DetalleOrden(id_producto=productos[1].id, cantidad=1, subtotal=25.0)
             # Quesadilla: Asada, Harina, Salsa Roja
-            det.opciones = [opciones[1], opciones[5], opciones[2]]
+            det.opciones = [opciones[1], opciones[12], opciones[7]]
             detalles.append(det)
 
             det2 = models.DetalleOrden(id_producto=productos[2].id, cantidad=1, subtotal=30.0)
@@ -159,7 +171,7 @@ def seed(db: Session):
         if i % 3 == 0:
             det = models.DetalleOrden(id_producto=productos[3].id, cantidad=2, subtotal=21.0 * 2)
             # Volcán: Asada, Salsa Verde
-            det.opciones = [opciones[1], opciones[3]]
+            det.opciones = [opciones[1], opciones[8]]
             # Asada (+5) * 2
             det.subtotal = (21.0 + 5.0) * 2
             detalles.append(det)
